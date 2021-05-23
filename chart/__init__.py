69fe984666a71cafaf6fb7f77dfe9468400f4928
red5885 <red5885@garretson.home>
2021-05-22 19:21:00 -0500

    added page that pulls static data from a python method and displays it in a chart

--- chart/__init__.py
+++ chart/__init__.py
@@ -3,4 +3,6 @@ from flask import Blueprint
 
 bp = Blueprint('charts', __name__, url_prefix='/chart')
 
-bp.add_url_rule('/', view_func=test_chart)
+bp.add_url_rule('/hard', view_func=test_chart)
+bp.add_url_rule('/fetch', view_func=fetch_chart)
+bp.add_url_rule('/data', view_func=get_data)
--- chart/views.py
+++ chart/views.py
@@ -1,4 +1,15 @@
-from flask import render_template
+from flask import render_template, jsonify
 
 def test_chart():
     return render_template('chart.html')
+
+def fetch_chart():
+    return render_template('datafetch.html')
+
+def get_data():
+    data = {
+        'x': [1,2,3,4,5],
+        'y1': [1,2,4,8,16],
+        'y2': [1,2,3,5,8]
+    }
+    return jsonify(data)
